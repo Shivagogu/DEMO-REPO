b3e80c01fd9c4e90b78f550f9c71ed716a80d20e
Shivagogu <gsivakotaiah84@gmail.com>
2024-09-18 12:24:53 +0530

    Add files via upload
    COMMIT-1

---
+++ big number.java
@@ -0,0 +1,14 @@
+//to find big number
+input java.lang.*;
+class big number{
+public static void main(string args[]){
+int a,b;
+System.out.println("enter a value");
+a=s.nextint();
+System.out.println("enter b value");
+b=s.nextint();
+if (a>b)
+{
+a is the biggest number
+}
+else
